7d6960cd12a089f363df1ac26aa9c431503be450
Mahi-Ali <164639420+Mahi-Ali@users.noreply.github.com>
2024-05-13 16:07:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+alert("Message is display on screen")
+//  console.log("Mahik");
+//  console.log("MAhik Ali");
+
+// document.write("learn java")
+
+//  var firstname = "Mahik";
+//  console.log(firstname);
+//  var firstname = "ali";
+// console.log(firstname);
+
+//   var dateOfBirth; //decliar
+//  dateOfBirth = "18-july-2004"; //initialize
+//  console.log(dateOfBirth);
+
+//  var name = "Mahik Ali"
+//  var age = 19;
+//  var email = "mahikroshanali@gmail.com";
+//  console.log( "my name is" + name + " My age is" + age + "my email is" + );
+
+//  var Message = "Hello in Java World"
+//  alert(Message);
+//  var Hello;
+// console.log(hello); //undefined
+
+//  var lastname = "MAhik";
+//   var lastname = "ALi";
+//  console.log(lastname);
+
+//   let x = 12;
+//   let y = 3;
+//   console.log ( "The Addition of two number is" + (x + y));
+//   console.log ( "The Multipilacation of two number is" + (x * y));
+//   console.log ( "The subtraction of two number is" + (x - y));
+//   console.log ( "The division of two number is" + (x / y));
+//   console.log ( "The modulus of two number is" + (x % y));
+
+
+//   //legal variable
+//   let firstname;
+//   let lastname;
+//   let _dateOfBirth;
+// let $email;
+// let age;
+// let age21;
+// let &hello;
+// let /question;
+
+// //illegal variable
+// let 12;
+// let 12return;
+// let !hello;
+// let @34question;
+// let "wasfa";
+// let ?AudioDestinationNode;
+
+
+// let rose = "flower";
+// console.log(Rose);
+
+
+
+
+
+// let onevar = "one"; secondvar = "two"; thirdvar = " three";
+
+// console.log("myVAr");
+// console.log("hello");
+// console.log("question");
+// console.log("age");
+// console.log("firstname");
+
+// let costofticket = 600;
+// let buyingticket  =5;
+// let result = costofticket * buyingticket;
+// console.log("total cost of ticket:" + result);
+
+
+let username = prompt("your name is?");
+console.log(username);
+
+ 
